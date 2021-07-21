48f5c9443dfedfcc7d29bb7570297ab2e1390537
jeremybenzakein <jeremy.benzakein@360learning.com>
2021-07-21 12:00:55 +0200

    chore: setup eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,22 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: '@babel/eslint-parser',
-    requireConfigFile: false
-  },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
+    root: true,
+    env: {
+        browser: true,
+        node: true
+    },
+    parserOptions: {
+        parser: "@babel/eslint-parser",
+        requireConfigFile: false
+    },
+    extends: [
+        "@nuxtjs",
+        "plugin:nuxt/recommended"
+    ],
+    plugins: [
+    ],
+    // add your custom rules here
+    rules: {
+        quotes: ["error", "double"],
+        indent: ["error", 4]
+    }
 }
